6a459292a78a332aa429bb749926f5806554f879
John Parent <john.parent@kitware.com>
2021-02-26 18:11:56 -0500

    Use docker volumes

--- build_docker.sh
+++ build_docker.sh
@@ -8,8 +8,8 @@ docker pull quay.io/pypa/manylinux2014_x86_64:latest
 docker images
 mkdir -p wheels
 rm -f wheels/*.whl
-docker build --force-rm -t kwiver:wheel --output type=local,dest=wheels -f Dockerfile.wheel .
-
+docker build --force-rm -t kwiver/wheels -f Dockerfile.wheel .
+docker run -v $PWD/wheels:/vol --rm --entrypoint bash kwiver/wheels:latest -c 'cp --preserve=timestamps /kwiver/python/dist/*.whl /vol/. && chown '`id -u`':'`id -g`' /vol/*.whl'
 mkdir pages
 cp --preserve=timestamps wheels/*.whl pages/.
 python3 mkpypa.py

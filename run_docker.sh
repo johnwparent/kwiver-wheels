7fd11c721df6a2fc69498c13d589f9e8e8f3e64c
John Parent <john.parent@kitware.com>
2021-04-07 19:36:54 -0400

    Update docker run command

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ set -e
 
 mkdir -p wheels
 rm -f wheels/*.whl
-docker run -v $PWD/wheels:/vol --rm --entrypoint bash "$DOCKERHUB_USERNAME"/kwiver_py:latest -c 'cp --preserve=timestamps /kwiver/python/dist/*.whl /vol/. && chown '`id -u`':'`id -g`' /vol/*.whl'
+docker run -v $PWD/wheels:/vol --rm --entrypoint bash "$DOCKERHUB_USERNAME"/kwiver_py37:latest -c 'cp --preserve=timestamps /kwiver/python/dist/*.whl /vol/. && chown '`id -u`':'`id -g`' /vol/*.whl'
 mkdir pages
 cp --preserve=timestamps wheels/*.whl pages/.
 python3 mkpypa.py

883920e0a43feb1dc73819fe548f66e687e2ca3d
John Parent <john.parent@kitware.com>
2021-04-07 17:33:48 -0400

    CircleCI config updates

--- build_fletch.sh
+++ build_fletch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+FILE="fpyi_docker.tar"
 if [ -f "$FILE" ]; then
     # docker load image from stored cache
     docker load -q < $FILE
 else
     # docker generate from scratch
-    docker build --force-rm -t fletch_py -f Dockerfile.fletch .
+    docker build --force-rm -t fletch_py37 -f Dockerfile.fletch .
 fi

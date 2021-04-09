079947d8d15687c61c78b434d149778a885de93b
John Parent <john.parent@kitware.com>
2021-04-09 19:20:38 -0400

    Transition to github actions from circleci

--- build_fletch.sh
+++ build_fletch.sh
@@ -2,7 +2,7 @@
 FILE="fpyi_docker.tar"
 if [ -f "$FILE" ]; then
     # docker load image from stored cache
-    docker load -q < $FILE
+    docker load < $FILE
 else
     # docker generate from scratch
     docker build --force-rm -t fletch_py37 -f Dockerfile.fletch .

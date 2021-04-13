dcb5f47e0d319f96ef07fb23d65de34f9f72af3e
John Parent <john.parent@kitware.com>
2021-04-13 13:20:05 -0400

    Remove fletch cleanup step

--- run_docker.sh
+++ run_docker.sh
@@ -7,7 +7,7 @@ mkdir -p wheels
 rm -f wheels/*.whl
 # docker run -v /wheels:/vol --rm --entrypoint bash "$DOCKERHUB_USERNAME"/kwiver_py37:latest -c 'cp --preserve=timestamps /kwiver/python/dist/*.whl /vol/. && chown '`id -u`':'`id -g`' /vol/*.whl'
 imname=$(docker create "$DOCKERHUB_USERNAME"/kwiver_py37)
-docker cp $imname:/kwiver/python/dist/ /wheels
+docker cp $imname:/kwiver/python/dist/ wheels
 mkdir pages
 cp --preserve=timestamps wheels/*.whl pages/.
 python3 mkpypa.py

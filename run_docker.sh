72943515a0fccc3ad15136457b7b49b2a2520738
John Parent <john.parent@kitware.com>
2021-04-13 11:32:33 -0400

    Refactor to use docker cp

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,9 @@ set -e
 
 mkdir -p wheels
 rm -f wheels/*.whl
-docker run -v /wheels:/vol --rm --entrypoint bash "$DOCKERHUB_USERNAME"/kwiver_py37:latest -c 'cp --preserve=timestamps /kwiver/python/dist/*.whl /vol/. && chown '`id -u`':'`id -g`' /vol/*.whl'
+# docker run -v /wheels:/vol --rm --entrypoint bash "$DOCKERHUB_USERNAME"/kwiver_py37:latest -c 'cp --preserve=timestamps /kwiver/python/dist/*.whl /vol/. && chown '`id -u`':'`id -g`' /vol/*.whl'
+imname=$(docker create "$DOCKERHUB_USERNAME"/kwiver_py37)
+docker cp $imname:/kwiver/python/dist/ /wheels
 mkdir pages
 cp --preserve=timestamps wheels/*.whl pages/.
 python3 mkpypa.py
